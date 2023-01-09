a503e339371e8e1fe8b10d64ead1ea68382a403e
mehmetali10 <mhmtali2316@gmail.com>
2023-01-09 22:31:13 +0300

    change connection to mysql database

--- BlogData/Entity/BlogDbContext.cs
+++ BlogData/Entity/BlogDbContext.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
 
 namespace BlogData.Entity
 {
     public class BlogDbContext : DbContext
     {
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=MEHMETALI10\\SQLEXPRESS;Database=BlogDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 21));
+            optionsBuilder.UseMySql("server=quiself.cdanhxvffndf.us-east-1.rds.amazonaws.com;database=BlogDB;uid=admin;pwd=password123456789", serverVersion);
         }
-
         public DbSet<User> Users { get; set; }
         public DbSet<Article> Articles { get; set; }
     }
